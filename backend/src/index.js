e058fb04871cc5463524c055a42d4d78fedfe4af
pg147 <xyzprathamesh147@gmail.com>
2025-01-01 17:15:47 +0530

    Updated index.js"

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,25 @@
+// Dependencies
 import express from 'express';
 import dotenv from 'dotenv';
+
+// Libs
 import { connectDB } from './lib/db.js';
+
+// Routers
+import bookingRoutes from './routes/booking.route.js';
 
 dotenv.config(); // for using env variables
 
 const app = express(); // initialized express app
 const PORT = process.env.PORT_NUMBER;
 
-
 // Using json parser
 app.use(express.json());
 
 // Routes 
-app.use('/api', (req, res) => {
-    res.send('Hi');
-})
+app.use('/api', bookingRoutes);
 
-// Server started at
+// Server 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
     connectDB();
