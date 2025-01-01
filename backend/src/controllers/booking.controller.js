1d7c13081217e914fb1041331d841a4cb0cd71c6
pg147 <xyzprathamesh147@gmail.com>
2025-01-01 17:46:15 +0530

    DeleteBooking logic added

--- backend/src/controllers/booking.controller.js
+++ backend/src/controllers/booking.controller.js
@@ -1,17 +1,5 @@
 import Booking from '../models/booking.model.js'
 
-export const getBookings = async (req, res) => {
-    try {
-        const bookings = await Booking.find();
-
-        if (bookings) {
-            res.status(200).json(bookings);
-        }
-    } catch (error) {
-        console.log('Error fetching bookings', )
-    }
-}
-
 export const createBookings = async (req, res) => {
     const { name: guestName, guestsCount, bookingDate, bookingSlot } = req.body;
     
@@ -28,6 +16,31 @@ export const createBookings = async (req, res) => {
         res.status(201).json(newBooking);
     } catch (error) {
         console.log('Error creating a booking : ', error.message);
+        res.status(500).json({message: 'Internal server error!'});
+    }
+}
+
+export const getBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find();
+
+        if (bookings.length === 0) {
+            res.status(200).json({message : 'No bookings found'});
+        } else res.status(200).json(bookings);
+    } catch (error) {
+        console.log('Error fetching bookings', )
+    }
+}
+
+export const deleteBooking = async (req, res) => {
+    const { id:bookingId } = req.params;
+
+    try {
+        await Booking.deleteOne({_id: bookingId });
+        
+        res.status(202).json({message: `Booking ${bookingId} deleted!`});
+    } catch (error) {
+        console.log('Error deleting the booking : ', error.message);
         res.status(500).json({message: 'Internal server error!'});
     }
 }
